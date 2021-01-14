0d17ea1eb65e3a3eb15d45539d2431ecc9fb76b5
qwanysh <olimpsdu@gmail.com>
2021-01-14 16:41:54 +0600

    Add babel

--- gulpfile.js
+++ gulpfile.js
@@ -2,46 +2,60 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const babel = require('gulp-babel');
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const rename = require('gulp-rename');
-const browserSync = require('browser-sync').create();
+const del = require('del');
+const server = require('browser-sync').create();
+
+const paths = {
+  styles: {
+    src: 'src/scss/**/*.scss',
+    dest: 'dist/css',
+  },
+  scripts: {
+    src: 'src/js/**/*.js',
+    dest: 'dist/js',
+  },
+};
+
+const clean = () => del(['dist']);
 
 const compileStyles = () => {
   return gulp
-    .src('./src/scss/**/*.scss')
+    .src(paths.styles.src)
     .pipe(sourcemaps.init())
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(autoprefixer({ cascade: false }))
     .pipe(sourcemaps.write())
     .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('./dist/css'))
-    .pipe(browserSync.stream());
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(server.stream());
 };
 
 const compileScripts = () => {
   return gulp
-    .src('./src/js/**/*.js')
+    .src(paths.scripts.src)
+    .pipe(babel({ presets: ['@babel/preset-env'] }))
     .pipe(sourcemaps.init())
     .pipe(concat('bundle.min.js'))
     .pipe(uglify())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./dist/js'));
+    .pipe(gulp.dest(paths.scripts.dest));
 };
 
-const build = gulp.series(compileStyles, compileScripts);
+const build = gulp.series(clean, compileStyles, compileScripts);
 
 const serve = () => {
   build();
 
-  browserSync.init({ server: { baseDir: './' } });
+  server.init({ server: { baseDir: './' } });
   gulp.watch('./src/scss/**/*.scss', compileStyles);
-  gulp.watch('./*.html').on('change', browserSync.reload);
-  gulp
-    .watch('./src/js/**/*.js', compileScripts)
-    .on('change', browserSync.reload);
+  gulp.watch('./*.html').on('change', server.reload);
+  gulp.watch('./src/js/**/*.js', compileScripts).on('change', server.reload);
 };
 
 exports.serve = serve;
