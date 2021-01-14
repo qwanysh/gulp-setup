16ac9b59026ad380dbdb1edce72e2e76dba3c9c3
qwanysh <olimpsdu@gmail.com>
2021-01-14 15:15:20 +0600

    Add css autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,18 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
 
 const compileStyles = () => {
-  const options = {
-    outputStyle: 'compressed',
-  };
-
   return gulp
     .src('./src/scss/**/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass(options).on('error', sass.logError))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./dist/css'))
     .pipe(browserSync.stream());
@@ -25,13 +23,13 @@ const compileScripts = () => {
   return gulp
     .src('./src/js/**/*.js')
     .pipe(sourcemaps.init())
-    .pipe(uglify())
     .pipe(concat('bundle.min.js'))
+    .pipe(uglify())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./dist/js'));
 };
 
-const build = gulp.parallel(compileStyles, compileScripts);
+const build = gulp.series(compileStyles, compileScripts);
 
 const serve = () => {
   build();
